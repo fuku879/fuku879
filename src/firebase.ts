2d9819fd1c9db79e826d01252f09c0b604926ad6
fuku879 <78995662+fuku879@users.noreply.github.com>
2024-12-22 13:14:03 +0900

    firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,15 +8,15 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAFbZacUTS-YYrn94q3AOHWdhJzkz5XZr0",
-    authDomain: "fir-chat-app-master-dad5a.firebaseapp.com",
+    apiKey: "",
+    authDomain: "",
     databaseURL:
-        "https://fir-chat-app-master-dad5a-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "fir-chat-app-master-dad5a",
-    storageBucket: "fir-chat-app-master-dad5a.firebasestorage.app",
-    messagingSenderId: "238468462066",
-    appId: "1:238468462066:web:ebc4604bff6d0665e7276b",
-    measurementId: "G-Z8VTXVE1N3",
+        "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
 };
 
 // Initialize Firebase
